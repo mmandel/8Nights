43fd7be14d0780ccb572beec8dc36d41332619f2
mmandel <mike@mmandel.com>
2014-10-29 00:20:01 -0400

    more experiments with hue lights
    I tried animating them with the animator, but it turns out they cant be
    driven continuously.  instead I have an "effect" script that sets some
    state and a "transition time".  This allows for fairly rapid strobes and
    fades

--- Assets/Scripts/Hue/HueMessenger.cs
+++ Assets/Scripts/Hue/HueMessenger.cs
@@ -6,7 +6,15 @@ public class HueMessenger : MonoBehaviour {
    public string BridgeIP = "10.0.1.11";
    public string User = "newdeveloper";
 
+   [Space(10)]
+
+   public bool FireAndForget = true;
+
+   [Space(10)]
+
    public Light[] Lights = new Light[1];
+
+   public static HueMessenger Instance { get; private set; }
 
    [System.Serializable]
    public class Light
@@ -16,9 +24,32 @@ public class HueMessenger : MonoBehaviour {
       public Color color = Color.red;
       [Range(0.0f, 1.0f)]
       public float fade = 1.0f;
+      public float transitionTime = .25f;
+
+      public bool ShouldPush() { return (on != _lastOn) || (_lastFade != fade) || (_lastColor != color); }
+
+      public void Pushed()
+      {
+         _lastColor = color;
+         _lastOn = on;
+         _lastFade = fade;
+         _lastUpdateTime = Time.time;
+      }
+
+      public float TimeSinceLastUpdate() { return Time.time - _lastUpdateTime; }
+
+      private Color _lastColor = Color.black;
+      private float _lastFade = 0.0f;
+      private bool _lastOn = false;
+      private float _lastUpdateTime = 0.0f;
    }
 
    bool _isRequesting = false;
+
+   void Awake()
+   {
+      Instance = this;
+   }
 
 	// Use this for initialization
 	void Start () 
@@ -26,10 +57,31 @@ public class HueMessenger : MonoBehaviour {
       //StartCoroutine(TestHue());
 	}
 
+   public void SetState(int lightIdx, bool on, float fade, Color c, float transitionTime)
+   {
+      if ((lightIdx >= 0) && (lightIdx < Lights.Length))
+      {
+         Lights[lightIdx].on = on;
+         Lights[lightIdx].fade = fade;
+         Lights[lightIdx].color = c;
+         Lights[lightIdx].transitionTime = transitionTime;
+      }
+   }
+
    void Update()
    {
-      if (!_isRequesting)
-         StartCoroutine(UpdateLights());
+      if (FireAndForget)
+      {
+         foreach (Light l in Lights)
+         {
+            UpdateLight(l);
+         }
+      }
+      else
+      {
+         if (!_isRequesting)
+            StartCoroutine(UpdateLights());
+      }
    }
 
    public IEnumerator UpdateLights()
@@ -38,28 +90,46 @@ public class HueMessenger : MonoBehaviour {
 
       foreach (Light l in Lights)
       {
-         string apiCall = "/api/" + User + "/lights/" + l.id +  "/state";
-         HSBColor hsbColor = new HSBColor(l.color);
-         string body = "{\"on\": " + ((l.on && (l.fade > 0.0f)) ? "true" : "false") +
-                       "\"hue\": " + (int)(hsbColor.h * 65535.0f) +
-                       "\"sat\": " + (int)(hsbColor.s * 255.0f) +
-                       "\"bri\": " + (int)(hsbColor.b * l.fade * 255.0f) +
-                       "}";
-         string url = "http://" + BridgeIP + apiCall;
-         //Debug.Log("URL: " + url + " body: " + body);
-         HTTP.Request request = new HTTP.Request("put", "http://" + BridgeIP + apiCall, JSON.JsonDecode(body) as Hashtable);
-         request.Send();
+         HTTP.Request request = UpdateLight(l);
 
-         while (!request.isDone)
+         if (!FireAndForget)
          {
-            yield return null;
-         }
+            while (!request.isDone)
+            {
+               yield return null;
+            }
 
-         if (!request.response.Text.Contains("success"))
-            Debug.Log("Error updating light: " + request.response.Text);
+            if (!request.response.Text.Contains("success"))
+               Debug.Log("Error updating light: " + request.response.Text);
+         }
       }
 
       _isRequesting = false;
+   }
+
+   HTTP.Request UpdateLight(Light l)
+   {
+      if (!l.ShouldPush())
+         return null;
+
+      string apiCall = "/api/" + User + "/lights/" + l.id + "/state";
+      float fade = Mathf.Clamp01(l.fade);
+      HSBColor hsbColor = new HSBColor(l.color);
+      int transitionTime = (int)(l.transitionTime * 10.0f); //this is specified in hundreds of millisecs (i.e 10 = 1000 ms = 1s)
+      string body = "{\"on\": " + ((l.on && (fade > 0.0f)) ? "true" : "false") +
+                    " \"hue\": " + (int)(hsbColor.h * 65535.0f) +
+                    " \"sat\": " + (int)(hsbColor.s * 255.0f) +
+                    " \"bri\": " + (int)(hsbColor.b * fade * 255.0f) +
+                    " \"transitiontime\": " + transitionTime +
+                    "}";
+      string url = "http://" + BridgeIP + apiCall;
+      Debug.Log("URL: " + url + " body: " + body + " at Time: " + Time.time + " deltaSinceLastUpdate: " + l.TimeSinceLastUpdate() + "\n");      
+      HTTP.Request request = new HTTP.Request("put", "http://" + BridgeIP + apiCall, JSON.JsonDecode(body) as Hashtable);
+      request.Send();
+
+      l.Pushed();      
+
+      return request;
    }
 
    public IEnumerator TestHue()
