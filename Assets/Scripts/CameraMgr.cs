6f28931d6cf4c6c8207f2757831dbfcc18f91d73
mmandel <mike@mmandel.com>
2014-11-28 19:38:29 -0500

    rift camera added
    I added the rift camera to our scene, which you can toggle to and from
    with the "C" key.  The arrow keys and left analog stick also rotate the
    camera, in case you don't have a headset

---
+++ Assets/Scripts/CameraMgr.cs
@@ -0,0 +1,85 @@
+﻿//
+//  Takes care of debug rotating camera with gamepad and switching between normal + rift cameras
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraMgr : MonoBehaviour 
+{
+
+   public Transform DebugRotateTrans;
+   public GameObject RiftCamera;
+   public GameObject NormalCamera;
+   public OVRManager RiftMgr;
+   public bool StartWithRiftCam = false;
+   public float DebugGamepadSensitivity = 2.5f;
+
+   private bool _riftCamActivated = false;
+   private Vector3 _debugRotEuler = Vector3.zero;
+   private float _minimumRotY = -75F;
+   private float _maximumRotY = 75F;
+
+   public static CameraMgr Instance { get; private set; }
+
+   void Awake()
+   {
+      Instance = this;
+   }
+
+	void Start () 
+   {
+      ActivateRiftCam(StartWithRiftCam);
+	}
+
+   void ActivateRiftCam(bool b)
+   {
+      _riftCamActivated = b;
+
+
+      RiftMgr.enabled = _riftCamActivated;
+
+      //RiftCamera.SetActive(_riftCamActivated);
+      //NormalCamera.SetActive(!_riftCamActivated);
+   }
+
+   void LateUpdate()
+   {
+      //toggle rift / normal camera mode
+      if (Input.GetKeyDown(KeyCode.C))
+         ActivateRiftCam(!_riftCamActivated);
+   }
+	
+	void Update () 
+   {
+      //toggle rift / normal camera mode
+      //if (Input.GetKeyDown(KeyCode.C))
+      //   ActivateRiftCam(!_riftCamActivated);
+
+      //recenter rift
+      if (Input.GetKeyDown(KeyCode.Space))
+         RecenterCamera();
+
+      float lStickX = Input.GetAxis("Horizontal");
+      if (!Mathf.Approximately(lStickX, 0.0f))
+      {
+         _debugRotEuler.y = _debugRotEuler.y + (lStickX * DebugGamepadSensitivity);
+      }
+      float lStickY = Input.GetAxis("Vertical");
+      if (!Mathf.Approximately(lStickY, 0.0f))
+      {
+         float newVal = _debugRotEuler.x - (lStickY * DebugGamepadSensitivity);
+         newVal = Mathf.Clamp(newVal, _minimumRotY, _maximumRotY);
+         _debugRotEuler.x = newVal;
+      }
+
+      DebugRotateTrans.localEulerAngles = _debugRotEuler;
+	}
+
+   void RecenterCamera()
+   {
+      if(OVRManager.display != null)
+         OVRManager.display.RecenterPose();
+      _debugRotEuler = Vector3.zero;
+   }
+}
